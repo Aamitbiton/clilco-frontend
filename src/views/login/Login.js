01d94dcb06501f9acb023ed33fc9a741db833ae8
amit <amit@amitech.dev>
2022-03-23 10:59:34 +0200

    add material ui and create login component

--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -1,16 +1,24 @@
+import React from 'react';
+import {Link, useNavigate} from 'react-router-dom'
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import './login.css'
 
 
-export const Login = ()=>(
+export const Login = ()=>{
+    const navigate = useNavigate()
+    const goToHome = ()=>{
+        navigate('/')
+    }
+    return (
     <div className={'login-container'}>
         <Stack spacing={2} direction="row" >
         {['Google', 'Apple', 'Facebook'].map((connectMethod)=>
-                <Button variant="contained">{connectMethod}</Button>
+                <Button onClick={goToHome} variant="contained">{connectMethod}</Button>
         )}
         </Stack>
     </div>
 
 )
+}
 
