d426a065192f95bb2218a127f87ced138dbdf473
chaim <chaimt93@gmail.com>
2022-03-27 15:13:18 +0300

    change firebase config

--- src/firebase/credentials.js
+++ src/firebase/credentials.js
@@ -1,12 +1,13 @@
 export default {
   config: {
-    apiKey: "AIzaSyDZ-dmpPh4DpW3-42uGUNDo0ZVgnZbz5ag",
-    authDomain: "clilco.firebaseapp.com",
-    projectId: "clilco",
-    storageBucket: "clilco.appspot.com",
-    messagingSenderId: "826783687000",
-    appId: "1:826783687000:web:42c480b7d2fc40d742852d",
-    measurementId: "G-X564SH50R9"
+    apiKey: "AIzaSyDDcmbGIo2J1lhbI7wJ6o2ctWVzxk2LPXY",
+    authDomain: "itserious-6c9dc.firebaseapp.com",
+    databaseURL: "https://itserious-6c9dc.firebaseio.com",
+    projectId: "itserious-6c9dc",
+    storageBucket: "itserious-6c9dc.appspot.com",
+    messagingSenderId: "127979555064",
+    appId: "1:127979555064:web:10f78653728bead85d130d",
+    measurementId: "G-4CJ5F48HKS"
   }
 }
 
