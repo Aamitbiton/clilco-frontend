097ccaf9d1b2ce5fa4e7cea60a30204967e82599
chaim <chaimt93@gmail.com>
2022-03-29 21:35:27 +0300

    watch rooms

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,10 @@
 import './App.css';
 import {Route, Routes} from "react-router-dom";
-import {AuthenticatedLayout} from './authenticatedLayout/AuthenticatedLayout'
-import {Login} from '../views/login/Login'
+import {AuthenticatedLayout} from './authenticatedLayout/AuthenticatedLayout';
+import {Login} from '../views/login/Login';
 import React, {useEffect, useState} from "react";
 import {init_app} from "../store/app/appFunctions";
 import {useSelector} from "react-redux";
-import {Lobby} from "../views/lobby/Lobby";
 
 
 function App() {
--- src/store/video/videoFunctions.js
+++ src/store/video/videoFunctions.js
@@ -8,5 +8,7 @@ const {getState, dispatch} = store
 export const watch_room = async () => {
     await videoService.watch_room((room)=>{
         //todo: if there is room - start video
+        // after date consider to delete the room or add parameter to stop listening
+        // maybe user the rooms as יומן שיחות
     })
 }
