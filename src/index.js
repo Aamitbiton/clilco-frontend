39e4a0b7d150edb36dba5b87d9e7753413fed9a2
amit <amit@amitech.dev>
2022-04-05 18:41:19 +0300

    login ui

--- src/index.js
+++ src/index.js
@@ -1,32 +1,29 @@
-import React,{Suspense} from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from "react-redux";
-import {store} from "./store";
-import './index.css';
-import App from './app/App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom'
-import { ThemeProvider } from '@mui/material/styles';
-import {theme} from './themes'
-import {Header} from "./app/header/header";
-const suspenseMarkup = ( <p>loading...</p> )
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import App from "./app/App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./themes";
+import { Header } from "./app/header/header";
+const suspenseMarkup = <p>loading...</p>;
 
 ReactDOM.render(
   <React.StrictMode>
-      <Provider store={store}>
-          <ThemeProvider theme={theme}>
-              <Suspense fallback={suspenseMarkup}>
-                  <BrowserRouter>
-                      <Header/>
-                      <App />
-                  </BrowserRouter>
-              </Suspense>
-
-          </ThemeProvider>
-      </Provider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Suspense fallback={suspenseMarkup}>
+          <BrowserRouter>
+            <Header />
+            <App />
+          </BrowserRouter>
+        </Suspense>
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
 
 reportWebVitals();
--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -1,37 +1,54 @@
-import React, {useState} from 'react';
-import Stack from '@mui/material/Stack';
-import {login_with_google, login_with_facebook} from '../../store/auth/authFunctions'
-import './login.css'
+import React, { useState } from "react";
+import Stack from "@mui/material/Stack";
+import {
+  login_with_google,
+  login_with_facebook,
+} from "../../store/auth/authFunctions";
+import "./login.css";
 import AppButton from "../../components/AppButton";
 import AppStack from "../../components/AppStack";
 import Title from "../../components/title/title";
-import googleIcon from '../../assets/Google-play-icon.png'
-import facebookIcon from '../../assets/Facebook-icon.png'
-import appleIcon from '../../assets/apple-icon.png'
-import emailIcon from '../../assets/clilco_logo_naked.png'
+import googleIcon from "../../assets/Google-play-icon.png";
+import facebookIcon from "../../assets/Facebook-icon.png";
+import appleIcon from "../../assets/apple-icon.png";
+import emailIcon from "../../assets/clilco_logo_naked.png";
 import LoginWithEmail from "./components/loginWithEmail";
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
 
-
-export const Login = ()=> {
-    const translate = useSelector(s=> s.app.global_hooks.translate)
-    const [enter_with_web, set_enter_with_web] = useState(false)
-    return (
-    <div className={'login-container'}>
-        <Title title={translate('Welcome_title')}/>
-        {
-            enter_with_web ?
-                <LoginWithEmail/>
-            :
-                <AppStack direction={'column'}>
-                    <AppButton endIcon={googleIcon} labelColor={'white'} label={"התחבר עם גוגל"} onClick={login_with_google} color="appTurquoise" />
-                    <AppButton endIcon={facebookIcon} labelColor={'white'} label={"התחבר עם פייסבוק"} onClick={login_with_facebook} color="appTurquoise" />
-                    <AppButton endIcon={appleIcon} labelColor={'white'} label={"התחבר עם אפל"}  color="appTurquoise" />
-                    <AppButton endIcon={emailIcon} labelColor={'white'} label={"התחבר עם שם משתמש"} onClick={()=> set_enter_with_web(true)} color="appTurquoise" />
-                </AppStack>
-
-        }
+export const Login = () => {
+  const translate = useSelector((s) => s.app.global_hooks.translate);
+  const [enter_with_web, set_enter_with_web] = useState(false);
+  return (
+    <div className={"login-container"}>
+      <Title title={translate("Welcome_title")} />
+      {enter_with_web ? (
+        <LoginWithEmail />
+      ) : (
+        <AppStack direction={"column"}>
+          <AppButton
+            endIcon={googleIcon}
+            labelColor={"white"}
+            label={"התחבר עם גוגל"}
+            onClick={login_with_google}
+          />
+          <AppButton
+            endIcon={facebookIcon}
+            labelColor={"white"}
+            label={"התחבר עם פייסבוק"}
+            onClick={login_with_facebook}
+          />
+          <AppButton
+            endIcon={appleIcon}
+            labelColor={"white"}
+            label={"התחבר עם אפל"}
+          />
+          <AppButton
+            endIcon={emailIcon}
+            labelColor={"white"}
+            label={"התחבר עם שם משתמש"}
+          />
+        </AppStack>
+      )}
     </div>
   );
 };
-
