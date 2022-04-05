17c77ac0d249e017f227a82ed255778e64140b12
amit <amit@amitech.dev>
2022-04-05 11:15:29 +0300

    login ui

--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Stack from '@mui/material/Stack';
 import {login_with_google, login_with_facebook} from '../../store/auth/authFunctions'
 import './login.css'
@@ -9,18 +9,26 @@ import googleIcon from '../../assets/Google-play-icon.png'
 import facebookIcon from '../../assets/Facebook-icon.png'
 import appleIcon from '../../assets/apple-icon.png'
 import emailIcon from '../../assets/clilco_logo_naked.png'
+import LoginWithEmail from "./components/loginWithEmail";
 
 
 export const Login = ()=>   {
+    const [enter_with_web, set_enter_with_web] = useState(false)
     return (
     <div className={'login-container'}>
         <Title title={'ברוכים הבאים'}/>
-        <AppStack direction={'column'}>
-                <AppButton startIcon={googleIcon} labelColor={'white'} label={"התחבר עם גוגל"} onClick={login_with_google} color="appTurquoise" />
-                <AppButton startIcon={facebookIcon} labelColor={'white'} label={"התחבר עם פייסבוק"} onClick={login_with_facebook} color="appTurquoise" />
-                <AppButton startIcon={appleIcon} labelColor={'white'} label={"התחבר עם אפל"}  color="appTurquoise" />
-                <AppButton startIcon={emailIcon} labelColor={'white'} label={"התחבר עם שם משתמש"}  color="appTurquoise" />
-        </AppStack>
+        {
+            enter_with_web ?
+                <LoginWithEmail/>
+            :
+                <AppStack direction={'column'}>
+                    <AppButton startIcon={googleIcon} labelColor={'white'} label={"התחבר עם גוגל"} onClick={login_with_google} color="appTurquoise" />
+                    <AppButton startIcon={facebookIcon} labelColor={'white'} label={"התחבר עם פייסבוק"} onClick={login_with_facebook} color="appTurquoise" />
+                    <AppButton startIcon={appleIcon} labelColor={'white'} label={"התחבר עם אפל"}  color="appTurquoise" />
+                    <AppButton startIcon={emailIcon} labelColor={'white'} label={"התחבר עם שם משתמש"} onClick={()=> set_enter_with_web(true)} color="appTurquoise" />
+                </AppStack>
+
+        }
     </div>
   );
 };
