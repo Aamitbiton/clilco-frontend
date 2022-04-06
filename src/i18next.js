485fdcb222b2e9c10f0176acc540b3477ca72845
amit <amit@amitech.dev>
2022-04-06 11:08:06 +0300

    login with password ui and i18next

--- src/i18next.js
+++ src/i18next.js
@@ -4,35 +4,30 @@ import { useTranslation, initReactI18next } from "react-i18next";
 import HttpApi from "i18next-http-backend";
 
 export const init_i18next = async () => {
-  const browser_lng = get_browser_lng();
+  const browser_lng = "en";
   await i18n
     .use(initReactI18next)
     .use(LanguageDetector)
     .use(HttpApi)
     .init({
       supportedLngs: ["en", "he"],
-      fallbackLng: browser_lng,
+      fallbackLng: "en",
       detection: {
-        order: [
-          "cookie",
-          "localStorage",
-          "sessionStorage",
-          "navigator",
-          "htmlTag",
-          "path",
-          "subdomain",
-        ],
+        order: ["cookie"],
         caches: ["cookie"],
       },
       backend: {
         loadPath: "/i18next/locales/{{lng}}/translation.json",
       },
     });
+  let t = await i18n.changeLanguage(browser_lng);
   set_direction(browser_lng);
-  return i18n.getFixedT();
+  return t;
 };
 const set_direction = (lng) => {
-  if (lng === "he") document.body.dir = "rtl";
+  if (lng === "he") {
+    document.body.dir = "rtl";
+  }
 };
 const get_browser_lng = () => {
   return window.navigator.language.split("-")[0] || "en";
