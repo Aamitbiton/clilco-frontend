f3f0108efe81cb83c0af21628b8f6bbf285ed530
chaim <chaimt93@gmail.com>
2022-04-07 17:22:49 +0300

    fixed mobile video style

--- src/views/videoDate/components/remoteVideo/RemoteVideo.js
+++ src/views/videoDate/components/remoteVideo/RemoteVideo.js
@@ -1,27 +1,52 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./remoteVideo.scss";
 import { useSelector } from "react-redux";
 
 export const RemoteVideo = ({ remoteStream }) => {
   const remoteVideoRef = useRef();
+  const [videoSize, setVideoSize] = useState({ width: 0, height: 0 });
   const isMobile = useSelector((state) => state.app.isMobile);
-  const get_video_class = () => {};
+  const set_video_size = () => {
+    setVideoSize({
+      width: remoteVideoRef.current.videoWidth,
+      height: remoteVideoRef.current.videoHeight,
+    });
+  };
   useEffect(() => {
     if (remoteStream) {
       remoteVideoRef.current.srcObject = remoteStream;
     }
   }, [remoteStream]);
+  useEffect(() => {
+    if (isMobile && videoSize.width > 450) {
+      //  mobile he is desktop
+    }
+    if (isMobile && videoSize.width < 450) {
+      // both mobile
+    }
+    if (!isMobile && videoSize.width < 450) {
+      // im  desktop he is mobile
+    }
+    if (!isMobile && videoSize.width > 450) {
+      // both  desktop
+    }
+  }, [videoSize]);
 
   return (
     <>
       {remoteStream && (
         <>
+          {/*<h1>{videoSize.width}</h1>*/}
+          {/*<h1>{videoSize.height}</h1>*/}
           <video
+            onLoadedData={set_video_size}
             playsInline
             ref={remoteVideoRef}
             autoPlay={true}
             className={`remote-video ${
-              isMobile ? "mobile-addons" : "desktop-addons"
+              isMobile && videoSize.width < videoSize.height
+                ? "both-mobile-video-style "
+                : ""
             }`}
             muted={true}
           />
