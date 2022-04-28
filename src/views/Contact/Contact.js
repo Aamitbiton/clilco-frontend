a58a3a0e134723fb5489a72a2c5b0c9d5bbbf8a1
chaim <chaimt93@gmail.com>
2022-04-28 13:17:09 +0300

    added report user

--- src/views/Contact/Contact.js
+++ src/views/Contact/Contact.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CenterLayout from "../../components/CenterLayout";
 import { Header } from "../home/components/header/header";
 import Title from "../../components/title/title";
@@ -11,53 +11,73 @@ import { send_contact_form } from "../../store/user/userFunctions";
 import { useSelector } from "react-redux";
 import defaultStyles from "../../style/defaultStyles";
 import { toast } from "react-toastify";
+import { useLocation } from "react-router-dom";
 
 function Contact(props) {
+  const location = useLocation();
   const { inputs } = defaultStyles;
   const [isLoading, setIsLoading] = useState(false);
+  const [initialValues, setInitialValues] = useState(null);
   const user = useSelector((s) => s.user.user);
+  const tranlate = useSelector((s) => s.app.global_hooks.translate);
   const SUCCESS_MESSAGE = "פנייתך נתקבלה בהצלחה.";
-  // const ERROR_MESSAGE = "ארעה שגיאה..";
   const handleSubmit = async (values) => {
     setIsLoading(true);
     const res = await send_contact_form({ user, ...values });
     res && (await toast(SUCCESS_MESSAGE, { type: "success" }));
     setIsLoading(false);
   };
+  const created = async () => {
+    load_initial_values();
+  };
+  const load_initial_values = () => {
+    const report_data = {
+      subject: tranlate("contact.report.subject"),
+      content:
+        tranlate("contact.report.content") +
+        " " +
+        location?.state?.report?.room?.id,
+    };
+    setInitialValues(
+      location.state?.report?.room ? report_data : { subject: "", content: "" }
+    );
+  };
+  useEffect(created, []);
   return (
     <>
       <Header />
       <CenterLayout direction={"column"}>
         <Title title={"צור קשר"} />
-        <AppForm
-          validationSchema={ContactValidation}
-          onSubmit={handleSubmit}
-          initialValues={{
-            subject: "",
-            content: "",
-          }}
-        >
-          <AppStack direction={"column"}>
-            <FormFiled
-              width={inputs.LARGE_INPUT_WIDTH}
-              name={"subject"}
-              label={"נושא הפניה"}
-            />
-            <FormFiled
-              width={inputs.LARGE_INPUT_WIDTH}
-              multiline={true}
-              rows={7}
-              name={"content"}
-              label={"תוכן הפניה"}
-            />
-            <SubmitButton
-              loadingButton={true}
-              loading={isLoading}
-              width={inputs.LARGE_INPUT_WIDTH}
-              label={"שלח"}
-            />
-          </AppStack>
-        </AppForm>
+        {initialValues && (
+          <AppForm
+            validationSchema={ContactValidation}
+            onSubmit={handleSubmit}
+            initialValues={initialValues}
+          >
+            <AppStack direction={"column"}>
+              <FormFiled
+                defaultValue={initialValues.subject}
+                width={inputs.LARGE_INPUT_WIDTH}
+                name={"subject"}
+                label={"נושא הפניה"}
+              />
+              <FormFiled
+                defaultValue={initialValues.content}
+                width={inputs.LARGE_INPUT_WIDTH}
+                multiline={true}
+                rows={7}
+                name={"content"}
+                label={"תוכן הפניה"}
+              />
+              <SubmitButton
+                loadingButton={true}
+                loading={isLoading}
+                width={inputs.LARGE_INPUT_WIDTH}
+                label={"שלח"}
+              />
+            </AppStack>
+          </AppForm>
+        )}
       </CenterLayout>
     </>
   );
