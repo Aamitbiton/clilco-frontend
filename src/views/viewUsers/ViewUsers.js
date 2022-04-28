1a911df3ef98aba7d09165e49d708da6220c2ecf
amit <aamitbiton@gmail.com>
2022-04-28 15:47:55 +0300

    finish view-users

--- src/views/viewUsers/ViewUsers.js
+++ src/views/viewUsers/ViewUsers.js
@@ -7,8 +7,12 @@ import "slick-carousel/slick/slick-theme.css";
 import { Header } from "../home/components/header/header";
 import Title from "../../components/title/title";
 import { useSelector } from "react-redux";
+import swipeAnimation from "../../assets/swipe_3.gif";
 
 export const ViewUsers = () => {
+  const isMobile = useSelector((state) => state.app.isMobile);
+  const swipeAnimation = require("../../assets/swipe_3.gif");
+  const loadingGiff = require("../../assets/loading.gif");
   const settings = {
     dots: false,
     infinite: true,
@@ -16,8 +20,10 @@ export const ViewUsers = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     rtl: false,
+    arrows: !isMobile,
   };
   const [users, setUsers] = useState(null);
+  const [view_animation, set_view_animation] = useState(true);
   const get_more_users = async () => {
     const newUsers = await f_get_all_users(!!users);
     const allUsers = users ? users.concat(newUsers) : newUsers;
@@ -29,9 +35,20 @@ export const ViewUsers = () => {
   return users ? (
     <div className={"flex-center"}>
       <Header />
+      {view_animation && (
+        <img
+          className="gif-swipe-animation"
+          src={swipeAnimation}
+          onAnimationEnd={() => {
+            set_view_animation(false);
+          }}
+        />
+      )}
+
       <div className={"carousel-container"}>
         <Slider
           beforeChange={(oldIndex, newIndex) => {
+            if (view_animation) set_view_animation(false);
             if (users.length - newIndex < 2) get_more_users();
           }}
           {...settings}
@@ -51,7 +68,9 @@ export const ViewUsers = () => {
       </div>
     </div>
   ) : (
-    <div>no users</div>
+    <div className={"flex-center"}>
+      <img src={loadingGiff} className="loading-giff"></img>
+    </div>
   );
 };
 
