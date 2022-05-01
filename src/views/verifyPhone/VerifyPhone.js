1bb8ef2ffd17129662019786c43c5cdc96905e76
Yakov Sinwani <yakovsinwani@gmail.com>
2022-05-01 13:45:34 +0300

    check if user already validate his phone

--- src/views/verifyPhone/VerifyPhone.js
+++ src/views/verifyPhone/VerifyPhone.js
@@ -1,18 +1,29 @@
 import React, { useState } from "react";
-import Button from "@mui/material/Button";
 import "./verifyPhone.css";
 import PhoneInput from "./components/phoneInput";
 import PasswordInput from "./components/passwordInput";
-import { send_sms, check_password } from "../../store/auth/authFunctions";
+import {
+  send_sms,
+  check_password,
+  signOut,
+} from "../../store/auth/authFunctions";
 import Title from "../../components/title/title";
 import CenterLayout from "../../components/CenterLayout";
+import { FORBIDDEN_MESSAGE, WRONG_PASSWORD_MESSAGE } from "./utils";
 
 export const VerifyPhone = () => {
   const [smsSent, setSmsSent] = useState(false);
   const [userPhoneNumber, setUserPhoneNumber] = useState(false);
+  const handle_existed_phone_validation = async () => {
+    alert(FORBIDDEN_MESSAGE);
+    await signOut();
+  };
   const send_code_sms = async (phoneNumber) => {
     if (!phoneNumber) phoneNumber = userPhoneNumber;
     let smsSendSuccesfully = await send_sms(phoneNumber);
+    if (smsSendSuccesfully.data.status === 403) {
+      return handle_existed_phone_validation();
+    }
     if (smsSendSuccesfully) {
       setUserPhoneNumber(phoneNumber);
       setSmsSent(true);
@@ -20,7 +31,7 @@ export const VerifyPhone = () => {
   };
   const check_if_password_verified = async (code) => {
     let code_verified = (await check_password(code)).data;
-    if (!code_verified) alert("סיסמא שגויה");
+    if (!code_verified) alert(WRONG_PASSWORD_MESSAGE);
   };
 
   return (
---
+++ src/views/verifyPhone/utils.js
@@ -0,0 +1,4 @@
+export const FORBIDDEN_MESSAGE =
+  "מספר הנייד שלך כבר אומת במערכת. אנא היכנס עם החשבון המשוייך למספר זה.\n ניתן לפנות לתמיכה בכתובת suppurt@clilco.com";
+
+export const WRONG_PASSWORD_MESSAGE = "סיסמא שגויה";
