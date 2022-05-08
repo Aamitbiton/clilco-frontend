fca451c02b18f786c36826736697af96fc11c62a
Yakov Sinwani <yakovsinwani@gmail.com>
2022-05-08 09:03:54 +0300

    testings

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./home.css";
 import { Header } from "./components/header/header";
 import MainView from "./components/MainView";
+import AppModal from "../../components/AppModal";
+import Typography from "@mui/material/Typography";
 
 export const Home = () => {
+  const [modalVisible, setModalVisible] = useState(true);
   return (
     <div className="home full-screen ">
       <Header />
       <div className="content">
+        <AppModal
+          modalVisible={modalVisible}
+          setModalVisible={() => setModalVisible(false)}
+        >
+          <Typography>HELLO SASSI!!!!</Typography>
+        </AppModal>
         <MainView />
       </div>
     </div>
