cc4b3b90d80057b61b3bfe75a3eac422b17558cb
chaim <chaimt93@gmail.com>
2022-05-12 15:23:06 +0300

    test 3.1

--- cypress/integration/test3/test3.spec.js
+++ cypress/integration/test3/test3.spec.js
@@ -1,17 +1,14 @@
 const user = "test2";
 const pass = "123456";
 const CYPRESS_BASE_URL = Cypress.env("CYPRESS_BASE_URL");
+console.log("000000000000000000000000000000000000000000000");
+console.log(CYPRESS_BASE_URL);
 
 describe(`test webrtc with user ${user}`, () => {
   it("should CYPRESS_BASE_URL", function () {
     cy.visit("https://www.google.com/");
-    console.log(
-      "***************************************************************"
-    );
     cy.get(`[data_cy=${CYPRESS_BASE_URL}]`).click();
   });
 
-  it("should test", function () {
-    cy.get(`[data_cy=${Cypress.env("TEST")}]`).click();
-  });
+  it("should test", function () {});
 });
