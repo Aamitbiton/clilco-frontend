79f94a6b76087817825df82ba69e134907cab9fb
chaim <chaimt93@gmail.com>
2022-05-12 13:06:01 +0300

    check if can pass variables

--- cypress/integration/test3/test3.spec.js
+++ cypress/integration/test3/test3.spec.js
@@ -1,7 +1,6 @@
 describe(`test webrtc with user ${user}`, () => {
-  it("should login with username and password", function () {
+  it("should get variable", function () {
     cy.visit(Cypress.env("CYPRESS_BASE_URL"));
     console.log(Cypress.env("CYPRESS_BASE_URL"));
-    debugger;
   });
 });
