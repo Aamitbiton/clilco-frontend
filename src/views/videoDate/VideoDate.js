bb421f8f430f26ae8ceafae174ce7e96c9e1c6cd
amit <aamitbiton@gmail.com>
2022-05-16 20:47:57 +0300

    finish view-users

--- src/views/videoDate/VideoDate.js
+++ src/views/videoDate/VideoDate.js
@@ -58,7 +58,7 @@ export const VideoDate = () => {
     }
   };
   const handle_no_remote_stream = () => {
-    if (remoteStream || window.rn_app.OS === "ios") return;
+    if (remoteStream || window.rn_app?.OS === "ios") return;
     infoLog("other user not in the date. refreshing every five minutes");
     [1, 2, 3, 4, 5, 6].forEach((number) => {
       setTimeout(() => {
