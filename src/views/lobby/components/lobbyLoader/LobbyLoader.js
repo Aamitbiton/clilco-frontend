551de13955a675fc68ac47f05f7519fe2bc0d839
amit <aamitbiton@gmail.com>
2022-06-22 13:10:43 +0300

    fix timer

--- src/views/lobby/components/lobbyLoader/LobbyLoader.js
+++ src/views/lobby/components/lobbyLoader/LobbyLoader.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './lobbyLoader.scss'
 import { CircularProgress } from "@mui/material";
 
 function LobbyLoader(props) {
+    const [currentDote, setCurrentDote] = useState(0)
+    useEffect(() => {
+        const timer = setInterval(() => {
+            change_dotes()
+        }, 500);
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
+    const change_dotes = () =>{
+        let current_dote = null;
+        setCurrentDote((value)=>{
+            current_dote = value
+        })
+        if (current_dote >= dotesArray.length - 1) setCurrentDote(0)
+        else setCurrentDote(current_dote + 1)
+    }
+    const dotesArray = ['.','..','...','....','.....','......','.......','........']
     return (
         <div className={'loader-container'}>
-            <CircularProgress size={50} />
-            <p className={'loader-text'}>מחפש התאמה...</p>
+            <p className={'loader-dotes'}>{dotesArray[currentDote]} </p>
+            <p className={'loader-text'}>מחפש התאמה </p>
+            <p className={'loader-dotes'}>{dotesArray[currentDote]} </p>
         </div>
     );
 }
