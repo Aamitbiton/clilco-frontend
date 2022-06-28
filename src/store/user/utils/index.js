bf35cc64b13a6cf966081436f0e2137dd029f733
amit <aamitbiton@gmail.com>
2022-06-28 17:43:58 +0300

    fix video

--- src/store/user/utils/index.js
+++ src/store/user/utils/index.js
@@ -1,10 +1,10 @@
 export function get_relevant_age_range({ birthday, gender, wanted }) {
   let years_to_reduce = 5;
-  if (gender !== wanted && gender === "female") years_to_reduce = 2;
+  if (gender !== wanted && gender === "female") years_to_reduce = 3;
   else if (gender !== wanted && gender === "male") years_to_reduce = 10;
   let years_to_increase = 5;
   if (gender !== wanted && gender === "female") years_to_increase = 10;
-  else if (gender !== wanted && gender === "male") years_to_increase = 2;
+  else if (gender !== wanted && gender === "male") years_to_increase = 3;
 
   const minAge = new Date(birthday).setFullYear(
     new Date(birthday).getFullYear() - years_to_reduce
