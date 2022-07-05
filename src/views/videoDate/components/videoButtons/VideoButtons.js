a938316dca4b48de8dd8eaf8adeaa3d5d7d91500
yakov <yakovsinwani@gmail.com>
2022-07-05 16:55:27 +0300

    fdsfs

--- src/views/videoDate/components/videoButtons/VideoButtons.js
+++ src/views/videoDate/components/videoButtons/VideoButtons.js
@@ -16,6 +16,14 @@ export const VideoButtons = ({
   const translate = useSelector((s) => s.app.global_hooks.translate);
   const isMobile = useSelector((s) => s.app.isMobile);
   const [disable, setDisable] = useState(false);
+  const handle_next_question_btn = () => {
+    if (disable) return;
+    setDisable(true);
+    next_question();
+    setTimeout(() => {
+      setDisable(false);
+    }, 1000);
+  };
   return (
     <>
       <div
@@ -55,13 +63,4 @@ export const VideoButtons = ({
       </div>
     </>
   );
-
-  const handle_next_question_btn = () => {
-    if (disable) return;
-    setDisable(true);
-    next_question();
-    setTimeout(() => {
-      setDisable(false);
-    }, 1000);
-  };
 };
